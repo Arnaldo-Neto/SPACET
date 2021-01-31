3c140946c2accf7270763652b27ae9d4b67b9c03
Arnaldo-Neto <78307498+Arnaldo-Neto@users.noreply.github.com>
2021-01-31 17:49:13 +0100

    Add files via upload

---
+++ SPACET/_cabecalho.php
@@ -0,0 +1,20 @@
+<?php
+//=============================================
+//cabecalho
+//==============================================
+?>
+
+
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SPACET</title>
+<!--CSS-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
+    <link  rel="stylesheet" href="main.css">
+</head>
+<body>
+    
+
---
+++ SPACET/index.php
@@ -0,0 +1,14 @@
+<?php
+
+//===========================================
+//index
+
+//===================
+include_once('_cabecalho.php');
+
+echo '<p>teste</p>';
+
+include_once('_rodape.php');
+
+
+?>
